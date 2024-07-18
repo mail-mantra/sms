563996922dd8ae3d00ec3f0ab8dc3ad9de58d194
milan-sahana <milansahana@gmail.com>
2024-07-18 22:53:26 +0530

    Update Sms.php
    -- sms Send

--- src/Sms.php
+++ src/Sms.php
@@ -32,16 +32,101 @@ class Sms
     }
 
 
-    public function send($to, $message)
+    public function send($to, $message, $dlt_te_id, $extras = null)
+    {
+        $result = [];
+        //Multiple mobiles numbers separated by comma
+        $mobileNumber = trim($to);
+        $message = urlencode($message);
+
+
+        $postData = array(
+            'authkey' => $this->authKey,
+            'mobiles' => $mobileNumber,
+            'message' => $message,
+            'DLT_TE_ID' => $dlt_te_id,
+            'response' => 'json'
+        );
+
+        if($extras['flash'] == 1) {
+            $postData['flash'] = 1;
+        }
+        if($extras['unicode'] == 1) {
+            $postData['unicode'] = 1;
+        }
+        if($extras['schtime']) {
+            $postData['schtime'] = $extras['schtime'];
+        }
+
+        //API URL
+        $url = "http://sms1.mailmantra.com/v2/api/send_sms";
+
+
+        // init the resource
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => http_build_query($postData)
+            //,CURLOPT_FOLLOWLOCATION => true
+        ));
+
+
+        //Ignore SSL certificate verification
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+
+        //get response
+        $output = curl_exec($ch);
+
+        //Print error if any
+        if(curl_errno($ch)) {
+            // echo 'error:' . curl_error($ch);
+            $result['status'] = 0;
+            $result['message'] = 'Internal Error'; // 'Curl Error..';
+            $result['code'] = 'S005';
+        }
+        else {
+
+            curl_close($ch);
+
+            // {"message":"376466724f44313832333830","type":"success"}
+            // {"message":"Authentication failure","type":"error"}
+            $output_arr = json_decode($output, true);
+
+
+            if($output_arr['status'] == 'success' || $output_arr['status'] == '1') {
+
+
+                $result['status'] = 1;
+                $result['message'] = 'SMS send Successfully..';
+                $result['code'] = $output_arr['message'];
+                //$result['output_arr']=$output_arr;
+
+            }
+            else {
+                $result['status'] = 0;
+                $result['message'] = $output_arr['message'];
+                $result['code'] = 'S002';
+                //$result['output_arr']=$output_arr;
+            }
+
+        }
+
+        $this->result = $result;
+
+        return $this->result;
+    }
+
+    public function sendBulk($to, $messages)
     {
         return true;
     }
 
-    public function sendBulk($to, $messages) {
-        return true;
-    }
-
-    public function balance() {
+    public function balance()
+    {
         $result = [];
         try {
             $postData = array(
